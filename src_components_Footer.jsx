b9e98bae8de067c80c4f81428ca682759332b58c
Mrigank Mathur <55250056+mrigankk20@users.noreply.github.com>
2021-06-05 19:58:51 +0530

    Add files via upload

---
+++ src_components_Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  var curYr = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright @ {curYr}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src_components_Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>React.js and Js</h1>
+      <p>
+        In 2020 React will continue to be the #1 contender in the front-end
+        space and will continue to do so for the foreseeable future, it is
+        simply too big to just die. ... Developers often pick React because it
+        is a safe bet right now.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
